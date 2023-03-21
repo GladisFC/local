e0c4104be5ba7d0ff84214273f28aab4db3eb721
GladisFC <flores.cespedes.91@gmail>
2023-03-21 12:21:10 -0300

    loginmodal

--- frontangular/src/app/app.module.ts
+++ frontangular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NadvarComponent } from './componentes/nadvar/nadvar.component';
 import { BotonloginComponent } from './componentes/botonlogin/botonlogin.component';
 import { BotoncerrarsesionComponent } from './componentes/botoncerrarsesion/botoncerrarsesion.component';
+import { ModalloginnadvarComponent } from './componentes/modalloginnadvar/modalloginnadvar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NadvarComponent,
     BotonloginComponent,
-    BotoncerrarsesionComponent
+    BotoncerrarsesionComponent,
+    ModalloginnadvarComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontangular/src/app/componentes/modalloginnadvar/modalloginnadvar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modalloginnadvar',
+  templateUrl: './modalloginnadvar.component.html',
+  styleUrls: ['./modalloginnadvar.component.css']
+})
+export class ModalloginnadvarComponent {
+
+}
